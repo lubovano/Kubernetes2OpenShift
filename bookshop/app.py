799a8f61eb8949bdd2287ad5969a0dfaeb5ffe10
Ľubomír Vaňo <buco@ceemel-2.local>
2019-04-23 11:10:15 +0200

    Pridaná kniha by buco...

--- bookshop/app.py
+++ bookshop/app.py
@@ -4,7 +4,7 @@ import requests
 
 
 app = Flask(__name__)
-BOOKS = ['1984', 'foo', 'bar', "ahoj a"]
+BOOKS = ['1984', 'foo', 'bar', "ahoj a", "Hitchhiker's Guide to Galaxy..."]
 
 @app.route("/")
 def hello():
